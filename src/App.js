b6272a4f7ea5ce63a3e937c59a9356a3ab92a6f4
takunagai <tak_na@icloud.com>
2022-04-25 18:01:29 +0900

    ボタンコンポーネント、画像コンポーネントの作成

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import './App.css'
+
 function Greeting({ name }) {
-  return <h1>Hello, {name}</h1>;
+  return <h1>Hello, {name}</h1>
 }
+
+function MyButton() {
+  return (
+    <button>Click me</button>
+  )
+}
+
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://picsum.photos/600/400.webp',
+  imageSize: 200,
+};
 
 const App = () => {
-  return <Greeting name="world" />
+  return <>
+    <Greeting name="world" />
+    <MyButton />
+    <img
+      className="avatar"
+      src={user.imageUrl}
+      alt={'photo of' + user.name}
+      style={{
+        width: user.imageSize,
+        height: user.imageSize
+      }}
+    />
+  </>
 }
 
-export default App;
+export default App
