4898293c79e879dc9dc2950d5b098c4a722c8279
takunagai <tak_na@icloud.com>
2022-04-26 21:24:58 +0900

    props を使用して親子コンポーネントで相互に通信させる

--- src/App3.js
+++ src/App3.js
@@ -1,5 +1,29 @@
 import Profile from './Profile.js'
 import TodoList from './TodoList.jsx'
+import { getImageUrl } from './utils'
+
+// 子要素 (Avatar) を受け取り、ラッパー要素で囲んで返す
+function Card({ children }) {
+  return(
+    <div className="card">
+      {children}
+    </div>
+  )
+}
+
+// props で受け取った値をセット
+// & データオブジェクトから取得した値をセットして画像を返す
+function Avatar({ person, size }) {
+  return(
+    <img
+      className="avatar"
+      src={getImageUrl(person)} // getImageUrl(person, 'l')
+      alt={person.name}
+      width={size}
+      height={size}
+    />
+  )
+}
 
 export default function App3() {
   return (
@@ -10,6 +34,16 @@ export default function App3() {
       <Profile />
       <hr />
       <TodoList />
+      <hr/>
+      <Card>
+        <Avatar
+          size={100}
+          person={{
+            name: 'katsuko Saruhashi',
+            imageId: 'YfeOqp2'
+          }}
+        />
+      </Card>
     </section>
   )
 }
