8c5702b641155abc6d87610a8a41834d3f80f8a5
takunagai <tak_na@icloud.com>
2022-04-26 21:35:32 +0900

    条件付きレンダリング

--- src/App3.js
+++ src/App3.js
@@ -1,5 +1,6 @@
 import Profile from './Profile.js'
 import TodoList from './TodoList.jsx'
+import { people } from './data.js'
 import { getImageUrl } from './utils'
 
 // 子要素 (Avatar) を受け取り、ラッパー要素で囲んで返す
@@ -25,7 +26,30 @@ function Avatar({ person, size }) {
   )
 }
 
+// 条件分岐レンダリング ( && )
+function Item({ name, isPacked }) {
+  return (
+    <li className="item">
+      {name} {isPacked && '✔'}
+    </li>
+  )
+}
+
 export default function App3() {
+
+  // リストをセット (オブジェクトデータをループ表示)
+  const listItems = people.map(person =>
+    <li key={person.id}>
+      <img src={getImageUrl(person)} alt={person.name}/>
+      <p>
+        <b>{person.name}</b>
+        {' ' + person.profession + ' '}
+        known for {person.accomplishment}
+      </p>
+    </li>
+  )
+
+  // レンダリング
   return (
     <section>
       <h1>Amazing scientists</h1>
@@ -44,6 +68,29 @@ export default function App3() {
           }}
         />
       </Card>
+
+      <div>
+        <h3>Sally Ride's Packing List</h3>
+        <ul>
+          <Item
+            isPacked={true}
+            name="Space suit"
+          />
+          <Item
+            isPacked={true}
+            name="Helmet with a golden leaf"
+          />
+          <Item
+            isPacked={false}
+            name="Photo of Tam"
+          />
+        </ul>
+      </div>
+
+      <div>
+        <h3>Scientists (オブジェクトデータをループ表示)</h3>
+        <ul>{listItems}</ul>
+      </div>
     </section>
   )
 }
