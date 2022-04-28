204967718bf3e2cae2080644b0fd309e12ff0bca
takunagai <tak_na@icloud.com>
2022-04-28 21:25:38 +0900

    条件付きレンダリング 1 - 基本5(事前に if 文で値を助県分岐させ主コードをシンプルに)

--- src/App5.jsx
+++ src/App5.jsx
@@ -1,7 +1,11 @@
 function Item({ name, isPacked }) {
+  let itemContent = name
+  if (isPacked) {
+    itemContent = name + '✅'
+  }
   return (
     <li className="item">
-      {name} {isPacked && '✅'}
+      {itemContent}
     </li>
   )
 }
