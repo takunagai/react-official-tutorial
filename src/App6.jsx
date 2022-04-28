efe670ab941625cee25e4ead4ac6d212eee17182
takunagai <tak_na@icloud.com>
2022-04-28 22:29:22 +0900

    リストのレンダリング2 - filter(), map() 科学者を抽出

--- src/App6.jsx
+++ src/App6.jsx
@@ -1,14 +1,78 @@
-const people = [
-  'Creola Katherine Johnson: mathematician',
-  'Mario José Molina-Pasquel Henríquez: chemist',
-  'Mohammad Abdus Salam: physicist',
-  'Percy Lavon Julian: chemist',
-  'Subrahmanyan Chandrasekhar: astrophysicist'
-]
+import { getImageUrl } from './utils'
+
+// /**
+//  * リストアイテムを表示
+//  */
+// const people = [
+//   'Creola Katherine Johnson: mathematician',
+//   'Mario José Molina-Pasquel Henríquez: chemist',
+//   'Mohammad Abdus Salam: physicist',
+//   'Percy Lavon Julian: chemist',
+//   'Subrahmanyan Chandrasekhar: astrophysicist'
+// ]
+//
+// export default function List() {
+//   const listItems = people.map(person =>
+//     <li>{person}</li>
+//   )
+//   return <ul>{listItems}</ul>
+// }
+
+
+/**
+ * リストアイテムをフィルタリングして表示 (科学者のみ抽出)
+ */
+const people = [{
+  id: 0,
+  name: 'Creola Katherine Johnson',
+  profession: 'mathematician',
+  accomplishment: 'spaceflight calculations',
+  imageId: 'MK3eW3A'
+}, {
+  id: 1,
+  name: 'Mario José Molina-Pasquel Henríquez',
+  profession: 'chemist',
+  accomplishment: 'discovery of Arctic ozone hole',
+  imageId: 'mynHUSa'
+}, {
+  id: 2,
+  name: 'Mohammad Abdus Salam',
+  profession: 'physicist',
+  accomplishment: 'electromagnetism theory',
+  imageId: 'bE7W1ji'
+}, {
+  id: 3,
+  name: 'Percy Lavon Julian',
+  profession: 'chemist',
+  accomplishment: 'pioneering cortisone drugs, steroids and birth control pills',
+  imageId: 'IOjWm71'
+}, {
+  id: 4,
+  name: 'Subrahmanyan Chandrasekhar',
+  profession: 'astrophysicist',
+  accomplishment: 'white dwarf star mass calculations',
+  imageId: 'lrWQx8l'
+}]
 
 export default function List() {
-  const listItems = people.map(person =>
-    <li>{person}</li>
+  // chemist だけを抽出
+  const chemists = people.filter(person =>
+    person.profession === 'chemist'
   )
+
+  const listItems = chemists.map(person =>
+    <li>
+      <img
+        src={getImageUrl(person)}
+        alt={person.name}
+      />
+      <p>
+        <b>{person.name}</b>
+        {` ${person.profession} `}
+        known for {person.accomplishment}
+      </p>
+    </li>
+  )
+
   return <ul>{listItems}</ul>
 }
