b17d183a810da591077d3b938ab2f49fdd4bdb93
takunagai <tak_na@icloud.com>
2022-04-30 16:02:59 +0900

    双方向性の追加 2 - ギャラリー(前後ボタン、説明文の開閉パネル)

--- src/App7.jsx
+++ src/App7.jsx
@@ -1,29 +1,41 @@
-export default function App() {
-  return (
-    <Toolbar
-      onPlayMovie={() => alert('Playing!')}
-      onUploadImage={() => alert('Uploading!')}
-    />
-  )
-}
+import { useState } from 'react' // 状態管理のフック
+import { sculptureList } from './data2'
 
-function Toolbar({ onPlayMovie, onUploadImage }) {
-  return (
-    <div>
-      <Button onClick={onPlayMovie}>
-        Play Movie {/* 子要素(children で渡す) */}
-      </Button>
-      <Button onClick={onUploadImage}>
-        Upload Image
-      </Button>
-    </div>
-  )
-}
+/**
+ * data2.js の各データの内容
+ * name: 'Homenaje a la Neurocirugía',
+ * artist: 'Marta Colvin Andrade',
+ * description: 'Although Colvin is predominantly known for abstract',
+ * url: 'https://i.imgur.com/Mx7dA2Y.jpg',
+ * alt: 'A bronze statue of two crossed hands delicately.'
+ */
+export default function Gallery() {
+  // 状態
+  const [index, setIndex] = useState(0) // 作品番号。初期値を設定
+  const [showMore, setShowMore] = useState(false) // 説明文の開閉状態
 
-function Button({ onClick, children }) {
+  function handleNextClick() {
+    setIndex(index + 1)
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore) // false/true トグル
+  }
+
+  let sculpture = sculptureList[index]
+
   return (
-    <button onClick={onClick}>
-      {children}
-    </button>
+    <>
+      <button onClick={handleNextClick}> {/* ()は付けたらダメ */}
+        next
+      </button>
+      <h2><i>{sculpture.name}</i> by {sculpture.artist}</h2>
+      <h3>({index + 1} of {sculptureList.length})</h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? 'Hide' : 'Show'} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
+      <img src={sculpture.url} alt={sculpture.alt} />
+    </>
   )
 }
