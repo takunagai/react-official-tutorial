06ea9b2f18ee4ef68862373632e1b3ecdf1a28b8
takunagai <tak_na@icloud.com>
2022-05-18 07:36:25 +0900

    宣言型 UI プログラミング1b - クイズフォーム (モックの作成2 分岐条件を手動で変えてテスト2)

--- src/Form8.js
+++ src/Form8.js
@@ -1,16 +1,25 @@
-export default function Form8({ status = 'empty' }) { // 'success'
+export default function Form8({
+  // Try: 'empty', 'submitting', 'success', 'error'
+  status = 'empty'
+}) {
   if (status === 'success') {
-    return <h1>That's right!</h1>
+    return <h1>正解！</h1>
   }
   return (
     <>
       <h2>City quiz</h2>
-      <p>In which city is there a billboard that turns air into drinkable water? (Answer: Lima)</p>
+      <p>空気を飲み水に変える看板があるのはどこの都市でしょう？ (答え: リマ)</p>
       <form>
-        <textarea />
+        <textarea disabled={status === 'submitting'} />
         <br />
-        <button>Submit</button>
+        <button disabled={
+          status === 'empty' || status === 'submitting'
+        }>Submit</button>
+        {status === 'error' &&
+          <p className="error">惜しいけど不正解。再チャレンジしてください。</p>
+        }
       </form>
+      <p><img src="https://beta.reactjs.org/_next/image?url=%2Fimages%2Fdocs%2Fdiagrams%2Fresponding_to_input_flow.dark.png&w=1920&q=75" width="500" /></p>
     </>
   )
 }
