9b22cf7337ef1958dadce9243c04abdd6a645990
takunagai <tak_na@icloud.com>
2022-05-24 07:57:06 +0900

    状態の重複を避ける - 修正前の状態変数の重複があるサンプル2 - 入力が下部のラベルに反映されない

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -10,13 +10,32 @@ export default function Menu() {
   const [items, setItems] = useState(initialItems)
   const [selectedItem, setSelectedItem] = useState(items[0]) // 上行の１アイテムと重複
 
+  function handleItemChange(id, e) {
+    setItems(items.map(item => {
+      if (item.id === id) {
+        return {
+          ...item,
+          title: e.target.value
+        }
+      } else {
+        return item
+      }
+    }))
+  }
+
   return (
     <>
       <h2>What's your travel snack?</h2>
       <ul>
-        {items.map(item => (
+        {items.map((item, index) => (
           <li key={item.id}>
-            {item.title}
+            <input
+              type="text"
+              value={item.title}
+              onChange={e => {
+                handleItemChange(item.id, e)
+              }}
+            />
             {' '}
             <button onClick={() => {
               setSelectedItem(item)
