6bc6646ff8e0f202b549d4b5f779f853c3ad08cb
takunagai <tak_na@icloud.com>
2022-05-26 21:28:44 +0900

    データオブジェクトの深いネストを避ける(自身、親の childIds 配列の2つのレベルの状態を更新) - 削除ボタンの追加

--- src/PlaceTree.jsx
+++ src/PlaceTree.jsx
@@ -1,20 +1,27 @@
 import { useState } from 'react'
 import { initialTravelPlan } from './placeTreeData'
 
-function PlaceTree({ id, placesById }) {
+function PlaceTree({ id, parentId, placesById, onComplete }) {
   const place = placesById[id]
   const childIds = place.childIds
 
   return (
     <>
-      <li>{place.title}</li>
+      <li>
+        {place.title}
+        <button onClick={() => {
+          onComplete(parentId, id)
+        }}>完了</button>
+      </li>
       {childIds.length > 0 && (
         <ol>
           {childIds.map(childId => (
             <PlaceTree
               key={childId} // リストに必要な key
               id={childId}
+              parentId={id}
               placesById={placesById}
+              onComplete={onComplete}
             />
           ))}
         </ol>
@@ -25,6 +32,21 @@ function PlaceTree({ id, placesById }) {
 
 export default function TravelPlan() {
   const [plan, setPlan] = useState(initialTravelPlan)
+
+  function handleComplete(parentId, childId) {
+    const parent = plan[parentId]
+    // この子 ID を含まない親の新バージョンを作成
+    const nextParent = {
+      ...parent,
+      childIds: parent.childIds.filter(id => id !== childId)
+    }
+    // ルートの状態オブジェクトを更新
+    setPlan({
+      ...plan,
+      [parentId]: nextParent // 更新された親を持つようにする
+    })
+  }
+
   const root = plan[0]
   const planetIds = root.childIds
 
@@ -36,7 +58,9 @@ export default function TravelPlan() {
           <PlaceTree
             key={id} // リストに必要な key
             id={id}
+            parentId={0}
             placesById={plan}
+            onComplete={handleComplete}
           />
         ))}
       </ol>
