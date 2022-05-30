aa1e5476d59197158b83f2ff7522c7c19c5d8bfb
takunagai <tak_na@icloud.com>
2022-05-30 23:10:12 +0900

    Try out some challenges 1b - 状態をリフトアップし同期 うまくいった

--- src/SyncedInputs.jsx
+++ src/SyncedInputs.jsx
@@ -1,29 +1,37 @@
 import { useState } from 'react'
 
 export default function SyncedInputs() {
-  return(
-    <>
-      <Input label="First input" /><br/>
-      <Input label="Second input" />
-    </>
-  )
-}
-
-function Input({ label }) {
   const [text, setText] = useState('')
 
   function handleChange(e) {
     setText(e.target.value)
   }
 
+  return(
+    <>
+      <Input
+        label="First input"
+        text={text}
+        onChange={handleChange}
+      /><br/>
+      <Input
+        label="Second input"
+        text={text}
+        onChange={handleChange}
+      />
+    </>
+  )
+}
+
+function Input({ label, text, onChange }) {
   return (
-    <label htmlFor="">
+    <label>
       {label}
       {' '}
       <input
         type="text"
         value={text}
-        onChange={handleChange}
+        onChange={onChange}
       />
     </label>
   )
