9b1f5d9573f16ef133b6896ac3d0c6b1f2c6210a
takunagai <tak_na@icloud.com>
2022-06-02 21:31:07 +0900

    Try out some challenges 4 - 「次へ」を押したら、すぐに前の画像を消す

---
+++ src/Gallery2.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+
+let images = [{
+  place: 'Penang, Malaysia',
+  src: 'https://i.imgur.com/FJeJR8M.jpg'
+}, {
+  place: 'Lisbon, Portugal',
+  src: 'https://i.imgur.com/dB2LRbj.jpg'
+}, {
+  place: 'Bilbao, Spain',
+  src: 'https://i.imgur.com/z08o2TS.jpg'
+}, {
+  place: 'Valparaíso, Chile',
+  src: 'https://i.imgur.com/Y3utgTi.jpg'
+}, {
+  place: 'Schwyz, Switzerland',
+  src: 'https://i.imgur.com/JBbMpWY.jpg'
+}, {
+  place: 'Prague, Czechia',
+  src: 'https://i.imgur.com/QwUKKmF.jpg'
+}, {
+  place: 'Ljubljana, Slovenia',
+  src: 'https://i.imgur.com/3aIiwfm.jpg'
+}]
+
+
+export default function Gallery2() {
+  const [index, setIndex] = useState(0)
+  const hasNext = index < images.length - 1
+
+  function handleClick() {
+    if (hasNext) {
+      setIndex(index + 1)
+    } else {
+      setIndex(0)
+    }
+  }
+
+  let image = images[index]
+  return (
+    <>
+      <button onClick={handleClick}>Next</button>
+      <h3>Image {index + 1} of {images.length}</h3>
+      <img key={index} src={image.src}  alt=""/>
+      <p>{image.place}</p>
+    </>
+  )
+}
