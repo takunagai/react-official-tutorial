23cb1e6155817266268c9cce60f9e079a2df3e01
takunagai <tak_na@icloud.com>
2022-06-06 23:04:19 +0900

    Reducer and Context 1d - TODOリスト (reducer と Context の両方を1つのファイルに移動し、コンポーネントをさらに整理)

--- src/TaskApp6.jsx
+++ src/TaskApp6.jsx
@@ -1,58 +1,13 @@
-import { useReducer } from 'react'
-import { TasksContext, TasksDispatchContext } from './TaskApp6TasksContext' // Context 読み込み
 import AddTask from './TaskApp6AddTask'
 import TaskList from './TaskApp6TaskList'
+import { TasksProvider } from './TaskApp6TasksContext'
 
-const initialTasks = [
-  { id: 0, text: 'Philosopher’s Path', done: true },
-  { id: 1, text: 'Visit the temple', done: false },
-  { id: 2, text: 'Drink matcha', done: false }
-]
-
-export default function TaskApp6() {
-  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks)
-
+export default function TaskApp() {
   return (
-    // Context Provider 2つを追加
-    <TasksContext.Provider value={tasks}>
-      <TasksDispatchContext.Provider value={dispatch}>
-        <h1>Day off in Kyoto</h1>
-        <AddTask />
-        <TaskList />
-      </TasksDispatchContext.Provider>
-    </TasksContext.Provider>
+    <TasksProvider>
+      <h1>Day off in Kyoto</h1>
+      <AddTask />
+      <TaskList />
+    </TasksProvider>
   )
-}
-
-/**
- * Reducer 関数 (状態ロジック)
- */
-function tasksReducer(tasks, action) {
-  switch (action.type) {
-    case 'added': {
-      return [
-        ...tasks,
-        {
-          id: action.id,
-          text: action.text,
-          done: false
-        }
-      ]
-    }
-    case 'changed': {
-      return tasks.map(t => {
-        if (t.id === action.task.id) {
-          return action.task
-        } else {
-          return t
-        }
-      })
-    }
-    case 'deleted': {
-      return tasks.filter(t => t.id !== action.id)
-    }
-    default: {
-      throw Error('Unknown action: ' + action.type)
-    }
-  }
 }
