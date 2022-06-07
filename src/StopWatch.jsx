de052412918b915615ef7e87120f3eec4634bd10
takunagai <tak_na@icloud.com>
2022-06-08 07:44:09 +0900

    コンポーネントに参照を追加 2 - ストップウォッチ(Stop 処理加え完成。インターバルIDニRef を使う)

--- src/StopWatch.jsx
+++ src/StopWatch.jsx
@@ -1,18 +1,25 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 export default function StopWatch() {
   const [startTime, setStartTime] = useState(null)
   const [now, setNow] = useState(null)
+  const intervalRef = useRef(null) // レンダリング不要なインターバルIDを参照に保存
 
   function handleStart() {
     setStartTime(Date.now())
     setNow(Date.now())
 
-    setInterval(() =>{
-        setNow(Date.now())
-      },
-      10 // ミリ秒
-    )
+    clearInterval(intervalRef.current)
+
+    // setInterval() は、繰り返し呼び出し＆インターバルを一意に識別するインターバル ID を返す
+    // そのインターバルIDを参照に保存しておく(clearInterval(インターバルID)で繰り返しストップ)
+    intervalRef.current = setInterval(() => {
+      setNow(Date.now())
+    }, 10) // ミリ秒
+  }
+
+  function handleStop() {
+    clearInterval(intervalRef.current)
   }
 
   let secondsPassed = 0
@@ -24,6 +31,7 @@ export default function StopWatch() {
     <>
       <h1>Time passed: {secondsPassed.toFixed(2)}</h1>
       <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
       <p><small>startTime: {startTime}<br />
         now: {now}</small></p>
     </>
