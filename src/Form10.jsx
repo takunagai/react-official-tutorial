21447873d9e6dd77f75b8021c37421acadf4a54f
takunagai <tak_na@icloud.com>
2022-06-09 12:02:33 +0900

    参照を利用した DOM の操作 1 - テキスト入力のフォーカス

--- src/Form10.jsx
+++ src/Form10.jsx
@@ -5,12 +5,12 @@ export default function Form10() {
 
   function handleClick() {
     inputRef.current.focus() // ブラウザ API のイベントが使える
-    // inputRef.current.remove() // ブラウザ API のイベントが使える
+    // inputRef.current.remove()
   }
 
   return (
     <>
-      <input ref={inputRef} /> {/* これでこの要素にブラウザ API のイベントを適用できる */}
+      <input ref={inputRef} /> {/* この要素を inputRef.current に配置 */}
       <button onClick={handleClick}>Focus the input</button>
     </>
   )
