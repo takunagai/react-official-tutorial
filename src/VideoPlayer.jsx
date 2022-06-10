f8594d180b0a83a120a9c7fea176c67aebe4d5c4
takunagai <tak_na@icloud.com>
2022-06-10 20:54:54 +0900

    Try out some challenges 1b - ビデオの再生/一時停止ボタン (完成)

--- src/VideoPlayer.jsx
+++ src/VideoPlayer.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 export default function VideoPlayer() {
   const [isPlaying, setIsPlaying] = useState(false)
+  let ref = useRef(null)
 
   function handleClick() {
     const nextIsPlaying = !isPlaying
     setIsPlaying(nextIsPlaying)
+    nextIsPlaying ? ref.current.play() : ref.current.pause()
   }
 
   return (
@@ -13,7 +15,12 @@ export default function VideoPlayer() {
       <button onClick={handleClick}>
         {isPlaying ? 'Pause' : 'Play'}
       </button>
-      <video width="250">
+      <video
+        width="250"
+        ref={ref}
+        onPlay={() => setIsPlaying(true)}
+        onPause={() => setIsPlaying(false)}
+      >
         <source
           src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
           type="video/mp4"
